369a79ec37d7714e2d4680f58817b7979852a6f5
최강훈 <ganghun@lepisode.team>
2025-12-24 17:28:37 +0900

    chore: reorganized directory structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export * from './lib/data-grid/data-grid.component';
-export * from './lib/data-grid/data-grid.type';
-export * from './lib/data-grid/rxdb.service';
-export * from './lib/data-grid/datagrid-storage.service';
+export * from './components/data-grid/data-grid.component';
+export * from './components/data-grid/data-grid.type';
