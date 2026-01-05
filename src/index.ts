716e4457d5fdae290d0a0bb0a82011647639b282
lepisodeyoungjin <youngjin@lepisode.team>
2026-01-05 10:35:01 +0900

    chore:update export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-export * from './libs/provideNgLepisode';
-export * from './libs/file-upload-service.interface';
+export * from "./libs/provideNgLepisode";
+export * from "./libs/file-upload-service.interface";
 
-export * from './components/data-grid';
-export * from './components/editor';
+export * from "./components/data-grid";
+
+export * from "./interactives/index";
